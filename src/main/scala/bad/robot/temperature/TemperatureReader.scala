6d205d651beec66d0543d13579b80be55e5133d5
Toby Weston <toby.weston@gmail.com>
2015-12-12 19:27:32 +0000

    adding something to read the temperature from a file

---
+++ src/main/scala/bad/robot/temperature/TemperatureReader.scala
@@ -0,0 +1,23 @@
+package bad.robot.temperature
+
+import bad.robot.temperature.ds18b20.Parser
+
+import scala.io.Source
+import scalaz.\/._
+import scalaz.syntax.std.option._
+
+
+object TemperatureReader {
+  def apply(filename: String) = new TemperatureReader(filename)
+}
+
+class TemperatureReader(filename: String) {
+
+  def read = {
+    for {
+      file <- fromTryCatchNonFatal(Source.fromFile(filename)).leftMap(FileError)
+      data <- file.getLines().toList.headOption.toRightDisjunction(UnexpectedError("Problem reading file, is it empty?"))
+      temperature <- Parser.parse(data)
+    } yield temperature
+  }
+}
