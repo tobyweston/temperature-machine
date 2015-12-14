f9f82b4dbe3a2bdb91f9b618d0f50a434d2313f4
Toby Weston <toby.weston@gmail.com>
2015-12-14 19:42:18 +0000

    Adding current time to example output

--- src/main/scala/bad/robot/temperature/TemperatureProbe.scala
+++ src/main/scala/bad/robot/temperature/TemperatureProbe.scala
@@ -1,5 +1,8 @@
 package bad.robot.temperature
 
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
 
 object TemperatureProbe {
   def apply() = new TemperatureProbe("/Users/toby/Workspace/bitbucket/temperature-machine/src/test/resources/examples/28-000005e2fdc2/w1_slave")
@@ -9,6 +12,12 @@ class TemperatureProbe(filename: String) extends Runnable {
 
   def run(): Unit = {
     val temperature = TemperatureReader(filename).read
-    temperature.foreach(temperature => println(temperature.celsius + " °C"))
+    temperature.foreach(temperature => println(s"$currentTime ${temperature.celsius} °C"))
+  }
+
+  def currentTime = {
+    val today = Calendar.getInstance().getTime()
+    val format = new SimpleDateFormat("dd/MM/YYYY HH:mm:ss")
+    format.format(today)
   }
 }
