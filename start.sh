69a0801e79d28d0b018449d027d62cea971eefa7
Toby Weston <toby.weston@gmail.com>
2016-01-01 20:46:13 +0000

    add IP to startup script; JMX will resolve to localhost otherwise

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-nohup java -Xmx512m -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=1616 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -jar target/scala-2.11/temperature-machine-1.0.jar > temperature-machine.log 2>&1 &
+IP=$( ifconfig wlan0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}' )
+nohup java -Xmx512m -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=1616 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=$IP -jar target/scala-2.11/temperature-machine-1.0.jar > temperature-machine.log 2>&1 &
+
 echo "$!" > temperature-machine.pid
 
 echo "Started your temperature-machine, redirecting output to temperature-machine.out, PID stored in temperature-machine.pid"
