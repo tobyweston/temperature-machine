05f8d598f844a4a843b61b244324ac5e59037304
Toby Weston <toby.weston@gmail.com>
2016-01-01 16:32:55 +0000

    Improve Main; trying to make it clearer if the sensor file can't be found

--- src/main/scala/bad/robot/temperature/Main.scala
+++ src/main/scala/bad/robot/temperature/Main.scala
@@ -1,7 +1,11 @@
 package bad.robot.temperature
 
+import java.io.File
+
 import bad.robot.temperature.TemperatureMachineThreadFactory._
-import bad.robot.temperature.rrd.RrdFile
+import bad.robot.temperature.ds18b20.SensorFile._
+import bad.robot.temperature.ds18b20.{SensorFile, SensorReader}
+import bad.robot.temperature.rrd.{Rrd, RrdFile}
 import bad.robot.temperature.server.Server
 
 import scala.concurrent.duration._
@@ -9,12 +13,21 @@ import scala.language.postfixOps
 
 object Main extends App {
 
-  RrdFile(30 seconds).create()
+  SensorFile.find().headOption match {
+    case Some(file) => start(file)
+    case _          => println(FailedToFindFile(BaseFolder).message)
+  }
 
-  Scheduler(30 seconds, createThreadPool("reading-thread")).start(Measurements.sensorToRrd())
-  Scheduler(90 seconds, createThreadPool("graphing-thread")).start(GenerateGraph(Duration(24, "hours")))
-  Scheduler(100 seconds, createThreadPool("xml-export-thread")).start(XmlExport(Duration(24, "hours")))
+  private def start(file: File) = {
+    print("RRD initialising...")
+    RrdFile(30 seconds).create()
 
-  Server(11900).start()
+    print("Starting monitoring...")
+    Scheduler(30 seconds, createThreadPool("reading-thread")).start(Measurement(SensorReader(file), Rrd()))
+    Scheduler(90 seconds, createThreadPool("graphing-thread")).start(GenerateGraph(Duration(24, "hours")))
+    Scheduler(100 seconds, createThreadPool("xml-export-thread")).start(XmlExport(Duration(24, "hours")))
 
+    print("Server initialising...")
+    Server(11900).start()
+  }
 }
