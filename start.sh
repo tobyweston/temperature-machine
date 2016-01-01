9b40ad3dd967a57cc2ccb7c712cf19f75783af31
Toby Weston <toby.weston@gmail.com>
2016-01-01 20:32:42 +0000

    -server flag not supported on the pi

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-nohup java -server -Xmx512m -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=1616 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -jar target/scala-2.11/temperature-machine-1.0.jar > temperature-machine.log 2>&1 &
+nohup java -Xmx512m -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=1616 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -jar target/scala-2.11/temperature-machine-1.0.jar > temperature-machine.log 2>&1 &
 echo "$!" > temperature-machine.pid
 
 echo "Started your temperature-machine, redirecting output to temperature-machine.out, PID stored in temperature-machine.pid"
