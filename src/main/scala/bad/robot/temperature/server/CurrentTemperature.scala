d19c04ffa16d4b37db9621ddd5a15d5e0b21290a
Toby Weston <toby.weston@gmail.com>
2016-01-02 18:34:55 +0000

    Show current temperature to 1 decimal place

--- src/main/scala/bad/robot/temperature/server/CurrentTemperature.scala
+++ src/main/scala/bad/robot/temperature/server/CurrentTemperature.scala
@@ -5,12 +5,16 @@ import bad.robot.temperature.ds18b20.SensorFile._
 import bad.robot.temperature.ds18b20.{SensorFile, SensorReader}
 import org.http4s.dsl._
 import org.http4s.server.HttpService
+import java.text.DecimalFormat
 
 import scalaz.syntax.std.option._
 
 object CurrentTemperature {
 
   private val sensor = SensorFile.find().headOption
+
+  private val formatter = new DecimalFormat("#.#")
+
 
   def service = HttpService {
 
@@ -20,7 +24,7 @@ object CurrentTemperature {
         temperature <- SensorReader(file).read
       } yield temperature
 
-      result.toHttpResponse(temperature => Ok(s"${temperature.celsius} °C"))
+      result.toHttpResponse(temperature => Ok(f"${temperature.celsius}%.1f °C"))
     }
 
   }
