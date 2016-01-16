9444ccf0b4a761ba1da7cab183b2abebe38f9302
Toby Weston <toby.weston@gmail.com>
2016-01-16 16:25:24 +0000

    Adding toHttpResponse test

--- src/test/scala/bad/robot/temperature/server/EitherToResponseTest.scala
+++ src/test/scala/bad/robot/temperature/server/EitherToResponseTest.scala
@@ -1,18 +1,27 @@
 package bad.robot.temperature.server
 
-import bad.robot.temperature.CrcFailure
+import java.lang.{Error => _}
+
+import bad.robot.temperature._
 import org.http4s.dsl._
 import org.specs2.mutable.Specification
 
-import scalaz.-\/
+import scala.{Error => _}
+import scalaz.{-\/, \/-}
 
 class EitherToResponseTest extends Specification {
 
+  val okResponse = (_: Any) => Ok()
+
   "Error mappings" >> {
-    -\/(CrcFailure).toHttpResponse(ko)
-    -\/(CrcFailure).toHttpResponse(temperatures => {
-      Ok()
-    })
+    -\/(CrcFailure()).toHttpResponse(okResponse).run.status                    must_== InternalServerError
+    -\/(SensorError("???")).toHttpResponse(okResponse).run.status              must_== InternalServerError
+    -\/(UnexpectedError("???")).toHttpResponse(okResponse).run.status          must_== InternalServerError
+    -\/(FailedToFindFile("???")).toHttpResponse(okResponse).run.status         must_== NotFound
+    -\/(FileError(new Exception("???"))).toHttpResponse(okResponse).run.status must_== InternalServerError
   }
 
+  "Success mapping" >> {
+    \/-("Well done").toHttpResponse(okResponse).run.status must_== Ok
+  }
 }
