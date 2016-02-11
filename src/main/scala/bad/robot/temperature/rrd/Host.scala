549dfdd468d01811d3541496e3864f2f6bf0fc29
Toby Weston <toby.weston@gmail.com>
2016-02-11 21:34:55 +0000

    trim hostnames that are too long for rrd4j

--- src/main/scala/bad/robot/temperature/rrd/Host.scala
+++ src/main/scala/bad/robot/temperature/rrd/Host.scala
@@ -6,4 +6,8 @@ object Host {
   def name = Host(InetAddress.getLocalHost.getHostName)
 }
 
-case class Host(name: String)
+case class Host(name: String) {
+
+  // another arbitrary constraint of rrd4j; data source names can only be a max of 20 characters
+  def trim(): Host = Host(name.take(20 - "-sensor-1".length))
+}
---
+++ src/test/scala/bad/robot/temperature/rrd/HostTest.scala
@@ -0,0 +1,12 @@
+package bad.robot.temperature.rrd
+
+import org.specs2.mutable.Specification
+
+class HostTest extends Specification {
+
+  "Trims to max 20 characters (including the 'sensor-n' postfix" >> {
+    Host("cheetah.local").trim must_== Host("cheetah.loc")
+    "cheetah.loc-sensor-1".length must_== 20
+  }
+
+}
