d933c49ac13121ff4bc312184a9b84db89abfa38
Toby Weston <toby.weston@gmail.com>
2016-02-11 21:38:42 +0000

    output errors on writing measurements

--- src/main/scala/bad/robot/temperature/task/RecordTemperature.scala
+++ src/main/scala/bad/robot/temperature/task/RecordTemperature.scala
@@ -8,6 +8,8 @@ import bad.robot.temperature.{Measurement, TemperatureReader, TemperatureWriter}
 
 case class RecordTemperature(input: TemperatureReader, output: TemperatureWriter, error: PrintStream = System.err) extends Runnable {
   def run(): Unit = {
-    input.read.fold(error.println, temperatures => output.write(Measurement(Host.name, now(), temperatures)))
+    input.read.fold(error.println, temperatures => {
+      output.write(Measurement(Host.name, now(), temperatures)).leftMap(error.println)
+    })
   }
 }
