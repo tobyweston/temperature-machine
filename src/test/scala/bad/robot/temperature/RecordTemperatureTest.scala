bee80b649c2b45fff9d66555780078780c30d97b
Toby Weston <toby.weston@gmail.com>
2016-02-11 21:40:05 +0000

    test for outputting errors encountered when writing measurements

--- src/test/scala/bad/robot/temperature/RecordTemperatureTest.scala
+++ src/test/scala/bad/robot/temperature/RecordTemperatureTest.scala
@@ -2,7 +2,6 @@ package bad.robot.temperature
 
 import java.io.{ByteArrayOutputStream, PrintStream}
 
-import bad.robot.temperature.rrd.Host
 import bad.robot.temperature.task.RecordTemperature
 import org.specs2.mutable.Specification
 
@@ -10,7 +9,7 @@ import scalaz.{-\/, \/-}
 
 class RecordTemperatureTest extends Specification {
 
-  "Take a measurement" >> {
+  "Take a measurement and write it out" >> {
     val input = new TemperatureReader {
       def read = \/-(List(Temperature(69.9)))
     }
@@ -21,7 +20,7 @@ class RecordTemperatureTest extends Specification {
         \/-(Unit)
       }
     }
-    RecordTemperature(input, output).run
+    RecordTemperature(input, output).run()
     output.temperatures must_== List(Temperature(69.9))
   }
 
@@ -34,7 +33,20 @@ class RecordTemperatureTest extends Specification {
     }
     val log = new ByteArrayOutputStream()
     val error = new PrintStream(log)
-    RecordTemperature(input, output, error).run
+    RecordTemperature(input, output, error).run()
+    log.toString must contain("UnexpectedError(whatever)")
+  }
+
+  "Take a measurement but fail to write it" >> {
+    val input = new TemperatureReader {
+      def read = \/-(List(Temperature(69.9)))
+    }
+    val output = new TemperatureWriter {
+      def write(measurement: Measurement) = -\/(UnexpectedError("whatever"))
+    }
+    val log = new ByteArrayOutputStream()
+    val error = new PrintStream(log)
+    RecordTemperature(input, output, error).run()
     log.toString must contain("UnexpectedError(whatever)")
   }
 
