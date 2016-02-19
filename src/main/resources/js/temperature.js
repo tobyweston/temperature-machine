6f505bc90af39309e4e76bfe4ab5a8f936ebb22a
Toby Weston <toby.weston@db.com>
2016-02-19 15:54:21 +0000

    adding first current temperature from the list of all temperatures. Refactoring of original behaviour only it gets the data from a cached server side value (an average of sensors for each host) rather than on-demand.
    Need to process the array of temperatures though so it displays the current temperature for each host, not just the first.
    
    Taters

--- src/main/resources/js/temperature.js
+++ src/main/resources/js/temperature.js
@@ -1,12 +1,17 @@
-function getCurrentTemperature() {
+function getCurrentTemperatures() {
   $.ajax({
-        method: "GET",
-        url: "temperature"
-      })
-      .done(function (temperature) {
-        $('#current-temperature').text(temperature);
-      })
-      .fail(function (jqXHR, textStatus) {
-        console.log("Failed to get /temperature " + textStatus);
-      })
+      method: "GET",
+      url: "temperatures",
+      dataType: "json"
+    })
+    .done(function (temperatures) {
+      var measurements = temperatures.measurements;
+      var celsius = Math.round(measurements[0].sensors[0].celsius * 10) / 10;
+      $('.temperature').text(celsius + " °C");
+      $('.source').text(measurements[0].host);
+      $('.updated').text(moment.unix(measurements[0].seconds).format('ddd HH:mm a'));
+    })
+    .fail(function (jqXHR, textStatus) {
+      console.log("Failed to get /temperature " + textStatus);
+    })
 }
