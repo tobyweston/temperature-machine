737c693d1dcef7ca394cd0525bf31ae0da8f580b
Toby Weston <toby.weston@gmail.com>
2016-02-21 20:11:32 +0000

    print n temperatures on the main page.

--- src/main/resources/js/temperature.js
+++ src/main/resources/js/temperature.js
@@ -6,10 +6,22 @@ function getCurrentTemperatures() {
     })
     .done(function (temperatures) {
       var measurements = temperatures.measurements;
-      var celsius = Math.round(measurements[0].sensors[0].celsius * 10) / 10;
-      $('.temperature').text(celsius + " °C");
-      $('.source').text(measurements[0].host);
-      $('.updated').text(moment.unix(measurements[0].seconds).format('ddd HH:mm a'));
+      if (measurements.length == 0)
+        $('#temperatures').append('<p>No readings</p>');
+
+      for (i = 0; i < measurements.length; i++) {
+        var celsius = Math.round(measurements[i].sensors[0].celsius * 10) / 10;
+        var host = measurements[i].host;
+        var lastUpdate = moment.unix(measurements[i].seconds).format('ddd HH:mm a');
+        var html =
+            '<div class="temperature">' +
+                '<h1><span class="temperature">' + celsius + ' °C</span></h1>' +
+                '<p class="source">' + host + '</p>' +
+                '<span class="updated small">updated: ' + lastUpdate + '</span>' +
+            '</div>';
+        $('#temperatures').append('' + html);
+      }
+
     })
     .fail(function (jqXHR, textStatus) {
       console.log("Failed to get /temperature " + textStatus);
