5d4bbf501fdc4720252f741dcf5c23a6cf3527f1
Toby Weston <toby.weston@db.com>
2016-03-02 13:00:26 +0000

    stop scheduled tasks from 'drifting'. Execute at fixed rate rather than fixed delay

--- src/main/scala/bad/robot/temperature/task/Scheduler.scala
+++ src/main/scala/bad/robot/temperature/task/Scheduler.scala
@@ -2,19 +2,15 @@ package bad.robot.temperature.task
 
 import java.util.concurrent.{ScheduledExecutorService, ScheduledFuture}
 
-import bad.robot.temperature.task.Scheduler._
-import org.http4s.util.task
-
 import scala.collection.JavaConverters._
 import scala.concurrent.duration.Duration
-import scalaz.concurrent.Task
 
 object Scheduler {
   def apply(frequency: Duration, executor: ScheduledExecutorService) = new Scheduler(frequency, executor)
 
   implicit class ScheduledExecutorServiceOps(executor: ScheduledExecutorService) {
-    def schedule(frequency: Duration, tasks: Runnable*) = {
-      tasks.map(task => executor.scheduleWithFixedDelay(wrapWithErrorHandler(task), 0, frequency.length, frequency.unit)).toList
+    def schedule(frequency: Duration, tasks: Runnable*): List[ScheduledFuture[_]] = {
+      tasks.map(task => executor.scheduleAtFixedRate(wrapWithErrorHandler(task), 0, frequency.length, frequency.unit)).toList
     }
   }
 
@@ -32,14 +28,6 @@ object Scheduler {
 }
 
 class Scheduler(frequency: Duration, executor: ScheduledExecutorService) {
-
-  def start(tasks: Runnable*): List[ScheduledFuture[_]] = {
-    tasks.map(task => executor.scheduleWithFixedDelay(wrapWithErrorHandler(task), 0, frequency.length, frequency.unit)).toList
-  }
-
-  def task(tasks: Runnable*): Task[List[ScheduledFuture[_]]] = Task.delay {
-    tasks.map(task => executor.scheduleWithFixedDelay(wrapWithErrorHandler(task), 0, frequency.length, frequency.unit)).toList
-  }
 
   def cancel(tasks: List[ScheduledFuture[_]]) {
     tasks.foreach(task => task.cancel(true))
