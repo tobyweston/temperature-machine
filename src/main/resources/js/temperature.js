0b1cd6fe04939825b33ea82caf3960666e1d05f7
Toby Weston <toby.weston@gmail.com>
2017-02-03 20:51:39 +0000

    #1 show individual sensor names if the url is /temperatures (or leave 'as is' if getting average temperatures).
    Change the ajax GET call's `url` parameter to "temperatures" to show individual temperatures.
    
    Just need to figure out how to toggle this within the UI.

--- src/main/resources/js/temperature.js
+++ src/main/resources/js/temperature.js
@@ -9,23 +9,30 @@ function getCurrentTemperatures() {
       if (measurements.length == 0)
         $('#temperatures').append('<p>No readings</p>');
 
-      for (i = 0; i < measurements.length; i++) {
-        var sensor = measurements[i].sensors[0];
-        var celsius = Math.round(sensor.temperature.celsius * 10) / 10;
-        var host = measurements[i].host;
-        var name = sensor.name;
-        var lastUpdate = moment.unix(measurements[i].seconds).format('ddd HH:mm a');
-        var html =
-            '<div class="temperature">' +
-                '<h1><span class="temperature">' + celsius + ' °C</span></h1>' +
-                '<p class="source">' + host + ' (' + name.toLowerCase() + ')</p>' +
-                '<span class="updated small">updated: ' + lastUpdate + '</span>' +
-            '</div>';
-        $('#temperatures').append('' + html);
-      }
+        for (i = 0; i < measurements.length; i++) {
+          var measurement = measurements[i];
+          var sensors = measurement.sensors;
+          for (j = 0; j < sensors.length; j++) {
+            var sensor = sensors[j];
+            var celsius = Math.round(sensor.temperature.celsius * 10) / 10;
+            var lastUpdate = moment.unix(measurement.seconds).format('ddd HH:mm a');
+            var sensorDescription = ''
+            if (sensors.length > 1) {
+              sensorDescription = '<div class="updated small">sensor : ' + sensor.name.toLowerCase() + '</div>'
+            }
 
-    })
-    .fail(function (jqXHR, textStatus) {
-      console.log("Failed to get /temperature " + textStatus);
-    })
+            var html =
+                '<div class="temperature">' +
+                '<h1><span class="temperature">' + celsius + ' °C</span></h1>' +
+                '<p class="source">' + measurement.host + '</p>' +
+                 sensorDescription +
+                '<div class="updated small">updated : ' + lastUpdate + '</div>' +
+                '</div>';
+            $('#temperatures').append('' + html);
+          }
+        }
+      })
+      .fail(function (jqXHR, textStatus) {
+        console.log("Failed to get /temperature/average " + textStatus);
+      })
 }
