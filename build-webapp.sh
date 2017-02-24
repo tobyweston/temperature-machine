1a3cfd67925904ba444fcabf9929942def12c507
Toby Weston <toby.weston@gmail.com>
2017-02-24 13:35:13 +0000

    tweaks to script

--- build-webapp.sh
+++ build-webapp.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e
+set -u
 
 # pre-conditions
 if ! git diff-index --quiet HEAD --; then
@@ -10,22 +12,27 @@ UI=target/web-app
 ROOT=`pwd`
 
 # download latest web-app
+echo "Downloading web-app..."
 rm -rf $UI
 mkdir -p $UI
 cd $UI
 git clone https://github.com/tobyweston/temperature-machine-ui.git
 
 # prepare web-app
+echo "Building web-app..."
 cd temperature-machine-ui
 npm update
 npm run build
 
 # copy into resources folder and sort git out
-cd ${ROOT}/src/main/resources
-git status | grep deleted | awk '{print $2}' | xargs git rm
+rm -rf ${ROOT}/src/main/resources
+mkdir -p ${ROOT}/src/main/resources
 cp -R build/ ${ROOT}/src/main/resources/
+
 cd ${ROOT}/src/main/resources/
+git status | grep deleted | awk '{print $2}' | xargs git rm
 git add .
 git commit -m "updated web-app from http://github.com/tobyweston/temperature-machine-ui"
 
 cd ${ROOT}
+echo "Ok"
