dd9ff6176d8144f523613ed090eb7f2acd991ac6
Toby Weston <toby.weston@gmail.com>
2017-03-11 20:28:25 +0000

    adding a test

--- src/test/scala/bad/robot/temperature/rrd/HostTest.scala
+++ src/test/scala/bad/robot/temperature/rrd/HostTest.scala
@@ -1,16 +1,22 @@
 package bad.robot.temperature.rrd
 
+import java.net.InetAddress
+
 import org.specs2.mutable.Specification
 
 class HostTest extends Specification {
 
-  "Trims to max 20 characters (including the 'sensor-n' postfix" >> {
+  "Trims to max 20 characters (including the 'sensor-n' postfix)" >> {
     Host("cheetah.local").trim must_== Host("cheetah.loc")
     "cheetah.loc-sensor-1".length must_== 20
   }
 
   "Doesn't trim" >> {
     Host("kitchen").trim must_== Host("kitchen")
+  }
+
+  "Local host" >> {
+    Host.local must_== Host(InetAddress.getLocalHost.getHostName)
   }
 
 }
