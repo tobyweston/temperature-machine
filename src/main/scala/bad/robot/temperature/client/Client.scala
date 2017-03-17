92b0ab72eaaf462b5ff0401e32823e2681a32228
Toby Weston <toby.weston@gmail.com>
2017-03-17 18:56:55 +0000

    client doesn't actually stay running since we defaulted all the threads to be daemon. introduce a latch to block the main thread (awaitShutdown). Can't actually clear the latch / shutdown gracefully so I'm not that happy

--- src/main/scala/bad/robot/temperature/client/Client.scala
+++ src/main/scala/bad/robot/temperature/client/Client.scala
@@ -1,5 +1,7 @@
 package bad.robot.temperature.client
 
+import java.util.concurrent.CountDownLatch
+
 import bad.robot.temperature.ds18b20.SensorFile
 import bad.robot.temperature.ds18b20.SensorFile._
 import bad.robot.temperature.rrd.Host
@@ -10,13 +12,18 @@ import scalaz.concurrent.Task
 
 object Client extends App {
 
-  val client: List[SensorFile] => Task[Unit] = sensors => {
+  private val latch = new CountDownLatch(1)
+
+  private val client: List[SensorFile] => Task[Unit] = sensors => {
     for {
       _      <- Task.delay(println(s"Initialising client '${Host.local.name}' (with ${sensors.size} of a maximum of $MaxSensors sensors)..."))
       server <- Task.delay(DiscoveryClient.discover)
-      tasks  <- Tasks.record(Host.local.trim, sensors, HttpUpload(server))
+      _      <- Tasks.record(Host.local.trim, sensors, HttpUpload(server))
+      _      <- awaitShutdown()
     } yield ()
   }
+
+  private def awaitShutdown(): Task[Unit] = Task.delay(latch.await())
 
   findSensorsAndExecute(client).leftMap(error => println(error.message))
 
