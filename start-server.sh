3cfa7ff4b1866dc1f081cd744a343a010e136acb
Toby Weston <toby.weston@gmail.com>
2017-03-26 11:20:04 +0100

    forgot to update sh. Fixes startup script

--- start-server.sh
+++ start-server.sh
@@ -3,7 +3,7 @@
 args=("$@")
 
 IP=$( ifconfig wlan0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}' )
-nohup java -Xmx512m -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=1616 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=$IP -cp target/scala-2.11/temperature-machine-2.0.jar bad.robot.temperature.server.Server $@ > temperature-machine.log 2>&1 &
+nohup java -Xmx512m -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=1616 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=$IP -cp target/scala-2.12/temperature-machine-2.0.jar bad.robot.temperature.server.Server $@ > temperature-machine.log 2>&1 &
 
 echo "$!" > temperature-machine.pid
 
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 IP=$( ifconfig wlan0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}' )
-nohup java -Xmx512m -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=1616 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=$IP -jar target/scala-2.11/temperature-machine-2.0.jar > temperature-machine.log 2>&1 &
+nohup java -Xmx512m -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=1616 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=$IP -jar target/scala-2.12/temperature-machine-2.0.jar > temperature-machine.log 2>&1 &
 
 echo "$!" > temperature-machine.pid
 
