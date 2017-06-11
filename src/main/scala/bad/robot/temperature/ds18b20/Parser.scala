bf10a848a8be3e11ebdd26d6d13a52bd499dd9af
Toby Weston <toby.weston@gmail.com>
2017-06-11 21:35:40 +0100

    fixes #13 support negative temperatures

--- src/main/scala/bad/robot/temperature/ds18b20/Parser.scala
+++ src/main/scala/bad/robot/temperature/ds18b20/Parser.scala
@@ -13,15 +13,15 @@ object Parser {
   val CrcByte = Hex
   val Crc = ": (crc=[0-9a-fA-F]{2})" + Space
   val CrcResult = "(YES|NO)"
-  val Rest = """([\s\w=]*)"""
+  val Rest = """([\s\w=-]*)"""
 
   val SensorOutput = (TempLsb + TempMsb + Hex + Hex + Hex + Hex + Hex + Hex + CrcByte + Crc + CrcResult + Rest).r
 
   def parse(content: String): Error \/ Temperature = {
     content match {
-      case SensorOutput(lsb, msb, _, _, _, _, _, _, _, _, "YES", _) => \/-(Temperature(Integer.parseInt(msb + lsb, 16) / 16.0))
-      case SensorOutput(_, _, _, _, _, _, _, _, _, _, "NO", _) => -\/(CrcFailure())
-      case data @ _ => -\/(UnexpectedError(s"Failed to recognise sensor data \n$data"))
+      case SensorOutput(lsb, msb, _, _, _, _, _, _, _, _, "YES", _) => \/-(Temperature(Integer.parseInt(msb + lsb, 16).shortValue / 16.0))
+      case SensorOutput(_, _, _, _, _, _, _, _, _, _, "NO", _)      => -\/(CrcFailure())
+      case data @ _                                                 => -\/(UnexpectedError(s"Failed to recognise sensor data \n$data"))
     }
   }
 }
--- src/test/scala/bad/robot/temperature/ds18b20/ParserTest.scala
+++ src/test/scala/bad/robot/temperature/ds18b20/ParserTest.scala
@@ -1,6 +1,6 @@
 package bad.robot.temperature.ds18b20
 
-import bad.robot.temperature.{UnexpectedError, CrcFailure, Temperature}
+import bad.robot.temperature.{CrcFailure, Temperature, UnexpectedError}
 import org.specs2.matcher.DisjunctionMatchers._
 import org.specs2.mutable.Specification
 
@@ -30,6 +30,50 @@ class ParserTest extends Specification {
     Parser.parse("af  fdsajhkl luasdfhjklf  asdf") must be_-\/.like {
       case error: UnexpectedError => ok
     }
+  }
+
+  "Low temperature" >> {
+    val output =
+      """|64 00 4b 46 7f ff 0c 10 3c : crc=3c YES
+         |64 00 4b 46 7f ff 0c 10 3c t=6250
+      """.stripMargin
+    Parser.parse(output) must be_\/-(Temperature(6.25))
+  }
+
+  "Negative temperatures (showing extra precision)" >> {
+    val output =
+      """|e1 ff 4b 46 7f ff 0c 10 fe : crc=fe YES
+         |e1 ff 4b 46 7f ff 0c 10 fe t=-1937
+      """.stripMargin
+    Parser.parse(output) must be_\/-(Temperature(-1.9375))
+  }
+
+  "Negative temperatures" >> {
+    val output =
+      """|7e ff 4b 46 7f ff 0c 10 c2 : crc=c2 YES
+         |7e ff 4b 46 7f ff 0c 10 c2 t=-8125
+      """.stripMargin
+    Parser.parse(output) must be_\/-(Temperature(-8.125))
+  }
+
+  // https://cdn-shop.adafruit.com/datasheets/DS18B20.pdf
+  "Various values from the spec sheet" >> {
+    Parser.parse(scratchPadWith("d0", "07")) must be_\/-(Temperature(125.0))
+    Parser.parse(scratchPadWith("50", "05")) must be_\/-(Temperature(85.0))
+    Parser.parse(scratchPadWith("91", "01")) must be_\/-(Temperature(25.0625))
+    Parser.parse(scratchPadWith("a2", "00")) must be_\/-(Temperature(10.125))
+    Parser.parse(scratchPadWith("08", "00")) must be_\/-(Temperature(0.5))
+    Parser.parse(scratchPadWith("00", "00")) must be_\/-(Temperature(0.0))
+    Parser.parse(scratchPadWith("f8", "ff")) must be_\/-(Temperature(-0.5))
+    Parser.parse(scratchPadWith("5e", "ff")) must be_\/-(Temperature(-10.125))
+    Parser.parse(scratchPadWith("6f", "fe")) must be_\/-(Temperature(-25.0625))
+    Parser.parse(scratchPadWith("90", "fc")) must be_\/-(Temperature(-55.0))
+  }
+
+  private def scratchPadWith(lsb: String, msb: String) = {
+    s"""|$lsb $msb 4b 46 7f ff 0c 10 fe : crc=fe YES
+        |$lsb $msb 4b 46 7f ff 0c 10 fe t=0
+     """.stripMargin
   }
 
 }
