fe598a9c0fa7bc40b782b335e7bd3da035db156b
Toby Weston <toby.weston@gmail.com>
2017-10-08 10:35:54 +0100

    #9 ignore spikes in temperatures

--- src/main/scala/bad/robot/temperature/SpikeIgnoringWriter.scala
+++ src/main/scala/bad/robot/temperature/SpikeIgnoringWriter.scala
@@ -1,9 +1,46 @@
 package bad.robot.temperature
 
+import bad.robot.temperature.SpikeIgnoringWriter._
+
+import scala.collection.concurrent.TrieMap
+
+object SpikeIgnoringWriter {
+  type SensorName = String
+
+  private val thirtyPercent = 30
+
+  // negative numbers would be a decrease, which we'll ignore (use Math.abs if we change our mind later)
+  def percentageIncrease(oldValue: Double, newValue: Double): Double = (newValue - oldValue) / oldValue * 100
+}
+
 class SpikeIgnoringWriter(delegate: TemperatureWriter) extends TemperatureWriter {
 
+  private val temperatures: TrieMap[SensorName, Temperature] = TrieMap()
+
   def write(measurement: Measurement) = {
-    delegate.write(measurement)
+
+//    val readings = measurement.temperatures.map(current => {
+//      val previous = temperatures.getOrElseUpdate(current.name, current.temperature)
+//      if (spikeBetween(current, previous)) {
+//        current.copy(temperature = previous)  // use old value
+//      } else {
+//        temperatures.update(current.name, current.temperature)
+//        current                               // pass down
+//      }
+//    })
+
+    val readings2 = measurement.temperatures.map(current => {
+      temperatures.get(current.name) match {
+        case Some(previous) if spikeBetween(current, previous) => current.copy(temperature = previous)
+        case _                                                 => temperatures.update(current.name, current.temperature); current
+      }
+    })
+
+    delegate.write(measurement.copy(temperatures = readings2))
+  }
+
+  private def spikeBetween(reading: SensorReading, previous: Temperature) = {
+    percentageIncrease(previous.celsius, reading.temperature.celsius) >= thirtyPercent
   }
 
 }
--- src/test/scala/bad/robot/temperature/SpikeIgnoreWriterTest.scala
+++ src/test/scala/bad/robot/temperature/SpikeIgnoreWriterTest.scala
@@ -10,14 +10,90 @@ class SpikeIgnoreWriterTest extends Specification {
   "Delegates" >> {
     val delegate = new StubWriter
     new SpikeIgnoringWriter(delegate).write(Measurement(Host("example"), Seconds(1), List(SensorReading("A", Temperature(21.1)))))
-    delegate.lastValue must beSome
+    delegate.temperatures must_== List(
+      Measurement(Host("example"), Seconds(1), List(SensorReading("A", Temperature(21.1))))
+    )
+  }
+
+  "Ignores spiked value (single sensor)" >> {
+    val delegate = new StubWriter
+    val writer = new SpikeIgnoringWriter(delegate)
+    writer.write(Measurement(Host("example"), Seconds(1), List(SensorReading("A", Temperature(21.1)))))
+    writer.write(Measurement(Host("example"), Seconds(2), List(SensorReading("A", Temperature(21.4)))))
+    writer.write(Measurement(Host("example"), Seconds(3), List(SensorReading("A", Temperature(21.6)))))
+    writer.write(Measurement(Host("example"), Seconds(4), List(SensorReading("A", Temperature(51.1)))))
+    delegate.temperatures must_== List(
+      Measurement(Host("example"), Seconds(1), List(SensorReading("A", Temperature(21.1)))),
+      Measurement(Host("example"), Seconds(2), List(SensorReading("A", Temperature(21.4)))),
+      Measurement(Host("example"), Seconds(3), List(SensorReading("A", Temperature(21.6)))),
+      Measurement(Host("example"), Seconds(4), List(SensorReading("A", Temperature(21.6))))
+    )
+  }
+
+  "Ignore spiked values (multiple sensors)" >> {
+    val delegate = new StubWriter
+    val writer = new SpikeIgnoringWriter(delegate)
+    writer.write(Measurement(Host("example"), Seconds(1), List(SensorReading("A", Temperature(21.1)))))
+    writer.write(Measurement(Host("example"), Seconds(3), List(SensorReading("A", Temperature(21.6)))))
+    writer.write(Measurement(Host("example"), Seconds(5), List(SensorReading("A", Temperature(21.1)))))
+    writer.write(Measurement(Host("example"), Seconds(7), List(SensorReading("A", Temperature(51.6)))))
+    writer.write(Measurement(Host("example"), Seconds(2), List(SensorReading("B", Temperature(31.4)))))
+    writer.write(Measurement(Host("example"), Seconds(4), List(SensorReading("B", Temperature(31.1)))))
+    writer.write(Measurement(Host("example"), Seconds(6), List(SensorReading("B", Temperature(31.4)))))
+    writer.write(Measurement(Host("example"), Seconds(8), List(SensorReading("B", Temperature(51.1)))))
+    delegate.temperatures must_== List(
+      Measurement(Host("example"), Seconds(1), List(SensorReading("A", Temperature(21.1)))),
+      Measurement(Host("example"), Seconds(3), List(SensorReading("A", Temperature(21.6)))),
+      Measurement(Host("example"), Seconds(5), List(SensorReading("A", Temperature(21.1)))),
+      Measurement(Host("example"), Seconds(7), List(SensorReading("A", Temperature(21.1)))),
+      Measurement(Host("example"), Seconds(2), List(SensorReading("B", Temperature(31.4)))),
+      Measurement(Host("example"), Seconds(4), List(SensorReading("B", Temperature(31.1)))),
+      Measurement(Host("example"), Seconds(6), List(SensorReading("B", Temperature(31.4)))),
+      Measurement(Host("example"), Seconds(8), List(SensorReading("B", Temperature(31.4))))
+    )
+  }
+
+  "Recovers from spiked value (single sensor)" >> {
+    val delegate = new StubWriter
+    val writer = new SpikeIgnoringWriter(delegate)
+    writer.write(Measurement(Host("example"), Seconds(1), List(SensorReading("A", Temperature(21.1)))))
+    writer.write(Measurement(Host("example"), Seconds(2), List(SensorReading("A", Temperature(21.4)))))
+    writer.write(Measurement(Host("example"), Seconds(3), List(SensorReading("A", Temperature(21.6)))))
+    writer.write(Measurement(Host("example"), Seconds(4), List(SensorReading("A", Temperature(51.1)))))
+    writer.write(Measurement(Host("example"), Seconds(5), List(SensorReading("A", Temperature(51.2)))))
+    writer.write(Measurement(Host("example"), Seconds(6), List(SensorReading("A", Temperature(51.5)))))
+    writer.write(Measurement(Host("example"), Seconds(7), List(SensorReading("A", Temperature(21.7)))))
+    delegate.temperatures must_== List(
+      Measurement(Host("example"), Seconds(1), List(SensorReading("A", Temperature(21.1)))),
+      Measurement(Host("example"), Seconds(2), List(SensorReading("A", Temperature(21.4)))),
+      Measurement(Host("example"), Seconds(3), List(SensorReading("A", Temperature(21.6)))),
+      Measurement(Host("example"), Seconds(4), List(SensorReading("A", Temperature(21.6)))),
+      Measurement(Host("example"), Seconds(5), List(SensorReading("A", Temperature(21.6)))),
+      Measurement(Host("example"), Seconds(6), List(SensorReading("A", Temperature(21.6)))),
+      Measurement(Host("example"), Seconds(7), List(SensorReading("A", Temperature(21.7))))
+    )
+  }
+
+  "Doesn't ignore negative spiked values (single sensor)" >> {
+    val delegate = new StubWriter
+    val writer = new SpikeIgnoringWriter(delegate)
+    writer.write(Measurement(Host("example"), Seconds(1), List(SensorReading("A", Temperature(21.1)))))
+    writer.write(Measurement(Host("example"), Seconds(2), List(SensorReading("A", Temperature(21.4)))))
+    writer.write(Measurement(Host("example"), Seconds(3), List(SensorReading("A", Temperature(21.6)))))
+    writer.write(Measurement(Host("example"), Seconds(4), List(SensorReading("A", Temperature(1.1)))))
+    delegate.temperatures must_== List(
+      Measurement(Host("example"), Seconds(1), List(SensorReading("A", Temperature(21.1)))),
+      Measurement(Host("example"), Seconds(2), List(SensorReading("A", Temperature(21.4)))),
+      Measurement(Host("example"), Seconds(3), List(SensorReading("A", Temperature(21.6)))),
+      Measurement(Host("example"), Seconds(4), List(SensorReading("A", Temperature(1.1))))
+    )
   }
 
   class StubWriter extends TemperatureWriter {
-    var lastValue: Option[Measurement] = None
+    var temperatures: List[Measurement] = List()
 
     def write(measurement: Measurement): \/[Error, Unit] = {
-      lastValue = Some(measurement)
+      temperatures = temperatures :+ measurement
       \/-(())
     }
   }
