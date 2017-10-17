f1c723f3a4b56cfb9c2c6c33b26a209b8eab5e70
Toby Weston <toby.weston@gmail.com>
2017-10-17 20:37:38 +0100

    #21 modify startup scripts to send the log file to the ~/.temperature folder where we should be able to serve it via HTTP

--- start-client.sh
+++ start-client.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+TEMPERATURE_HOME=~/.temperature
+LOG_FILE=${TEMPERATURE_HOME}/temperature-machine.log
 IP=$( ifconfig wlan0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}' )
-nohup java -Xmx512m -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=1616 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=$IP -cp target/scala-2.12/temperature-machine-2.1.jar bad.robot.temperature.client.Client > temperature-machine.log 2>&1 &
+
+mkdir ${TEMPERATURE_HOME} -p
+
+nohup java -Xmx512m -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=1616 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=${IP} -cp target/scala-2.12/temperature-machine-2.1.jar bad.robot.temperature.client.Client > ${LOG_FILE} 2>&1 &
+
+ln -s -F ${LOG_FILE} temperature-machine.log
 
 echo "$!" > temperature-machine.pid
-
-echo "Started your temperature-machine (client-mode), redirecting output to temperature-machine.log, PID stored in temperature-machine.pid"
+echo "Started your temperature-machine (client-mode);"
+echo "   Redirecting output to ${LOG_FILE}"
+echo "   PID stored in temperature-machine.pid"
