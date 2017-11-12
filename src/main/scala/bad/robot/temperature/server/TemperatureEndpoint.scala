a2b5ab6ad7fed410fcbb24d16b3e708ab79f54dd
Toby Weston <toby.weston@gmail.com>
2017-11-12 20:42:16 +0000

    basic websocket to give "live" temperatures back to the client (doesn't handly any client data)

--- src/main/scala/bad/robot/temperature/server/TemperatureEndpoint.scala
+++ src/main/scala/bad/robot/temperature/server/TemperatureEndpoint.scala
@@ -1,8 +1,8 @@
 package bad.robot.temperature.server
 
+import java.time.Clock
 import java.time.temporal.ChronoUnit.{MINUTES => minutes}
 import java.time.temporal.TemporalUnit
-import java.time.Clock
 
 import argonaut.Argonaut._
 import argonaut.EncodeJson
@@ -11,6 +11,14 @@ import bad.robot.temperature.rrd.Host
 import org.http4s.HttpService
 import org.http4s.dsl._
 import org.http4s.headers.`X-Forwarded-For`
+import org.http4s.server.websocket._
+import org.http4s.websocket.WebsocketBits._
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scalaz.concurrent.{Strategy, Task}
+import scalaz.stream.time.awakeEvery
+import scalaz.stream.{DefaultScheduler, Exchange, Process, Sink}
 
 object TemperatureEndpoint {
 
@@ -40,6 +48,18 @@ object TemperatureEndpoint {
 
     case GET -> Root / "temperatures" => {
       Ok(encode(current.filter(within(5, minutes))).spaces2)
+    }
+
+    case GET -> Root / "temperatures" / "live" => {
+      val source = awakeEvery(500 millis)(Strategy.DefaultStrategy, DefaultScheduler).map { d => 
+        Text(encode(current.filter(within(5, minutes))).spaces2) 
+      }
+
+      val sink: Sink[Task, WebSocketFrame] = Process.constant {
+        case Text(fromClient, _) => Task.delay(println(fromClient))
+        case f                   => Task.delay(println(s"Unknown type: $f"))
+      }
+      WS(Exchange(source, sink))
     }
 
     case DELETE -> Root / "temperatures" => {
