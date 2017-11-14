5a455388e48763872a21fb191a9b5836704fe0ab
Toby Weston <toby.weston@gmail.com>
2017-11-14 18:11:13 +0000

    update copy/deploy task to use ansible

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-# sbt assembly
-scp target/scala-2.12/temperature-machine-2.1.jar pi@study.local:/home/pi/code/temperature-machine/target/scala-2.12
-scp target/scala-2.12/temperature-machine-2.1.jar pi@kitchen.local:/home/pi/code/temperature-machine/target/scala-2.12
-scp target/scala-2.12/temperature-machine-2.1.jar pi@bedroom1.local:/home/pi/code/temperature-machine/target/scala-2.12
-scp target/scala-2.12/temperature-machine-2.1.jar pi@bedroom2.local:/home/pi/code/temperature-machine/target/scala-2.12
+
+# assumes you have Ansible setup and a 'ansible/inventory' file setup with machines grouped under 'temperatures'. See http://temperature-machine.com/docs/ansible.html
+
+sbt assembly && ansible -i ansible/inventory temperatures -m copy -a "src=target/scala-2.12/temperature-machine-2.1.jar dest=/home/pi/code/temperature-machine/target/scala-2.12" -u pi
