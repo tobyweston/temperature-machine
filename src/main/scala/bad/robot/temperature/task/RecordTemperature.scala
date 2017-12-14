ab42c1ffaf055bc20098606791344e53a9dbee75
Toby <toby.weston@db.com>
2017-12-14 12:16:04 +0000

    #21 move weird error logger class out

--- src/main/scala/bad/robot/temperature/task/RecordTemperature.scala
+++ src/main/scala/bad/robot/temperature/task/RecordTemperature.scala
@@ -1,11 +1,10 @@
 package bad.robot.temperature.task
 
-import java.io.{OutputStream, PrintStream}
+import java.io.PrintStream
 
 import bad.robot.temperature.rrd.Host
 import bad.robot.temperature.rrd.Seconds.now
 import bad.robot.temperature.{Error, Measurement, TemperatureReader, TemperatureWriter}
-import org.slf4j.Logger
 
 case class RecordTemperature(host: Host, input: TemperatureReader, output: TemperatureWriter, error: PrintStream = System.err) extends Runnable {
   def onError(stream: PrintStream): Error => Unit = error => stream.print(error + "\u0000")  
@@ -15,25 +14,4 @@ case class RecordTemperature(host: Host, input: TemperatureReader, output: Tempe
       output.write(Measurement(host, now(), temperatures)).leftMap(onError(error)); ()
     })
   }
-}
-
-import java.io.ByteArrayOutputStream
-
-object ErrorLogger {
-  
-  def apply(log: Logger = bad.robot.logging.Log): PrintStream = new PrintStream(new OutputStream {
-
-    private val stream = new ByteArrayOutputStream(1000)
-
-    def write(bytes: Int): Unit = {
-      if (bytes == '\u0000') {
-        val line = stream.toString
-        stream.reset()
-        log.error(line)
-      } else {
-        stream.write(bytes)
-      }
-    }
-  })
-
 }
