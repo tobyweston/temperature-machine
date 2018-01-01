5bf39c230a175d7f92201df3e4a98ed839d20ef0
Toby <toby.weston@gmail.com>
2018-01-01 22:25:10 +0000

    remove unused log config (left over from SLF4J)

--- src/main/scala/bad/robot/logging/package.scala
+++ src/main/scala/bad/robot/logging/package.scala
@@ -5,19 +5,8 @@ import org.apache.logging.log4j.LogManager
 
 package object logging {
 
-  sys.props +=
-    ("org.slf4j.simpleLogger.logFile"                   ->  RrdFile.path / "temperature-machine.log") +=
-    ("org.slf4j.simpleLogger.defaultLogLevel"           -> "error") +=
-    ("org.slf4j.simpleLogger.log.bad.robot.temperature" -> "info") +=
-    ("org.slf4j.simpleLogger.showDateTime"              -> "true") +=
-    ("org.slf4j.simpleLogger.dateTimeFormat"            -> "\u0000yyyy-MM-dd HH:mm:ss:SSS") +=
-    ("org.slf4j.simpleLogger.showThreadName"            -> "true") +=
-    ("org.slf4j.simpleLogger.showLogName"               -> "false") +=
-    ("org.slf4j.simpleLogger.showShortLogName"          -> "false")
+  sys.props += ("log.location" -> RrdFile.path / "temperature-machine.log")
 
-  sys.props +=
-    ("log.location"                                     -> RrdFile.path / "temperature-machine.log")
-  
   val Log = LogManager.getLogger("bad.robot.temperature")
 
 }
