4ae2d2ecdb615c47f525246d9adaafc508e0468a
Toby <toby.weston@gmail.com>
2018-02-07 19:03:51 +0100

    #55 change logged time to be UTC

--- src/main/scala/bad/robot/temperature/LogParser.scala
+++ src/main/scala/bad/robot/temperature/LogParser.scala
@@ -1,7 +1,6 @@
 package bad.robot.temperature
 
 import java.time.Instant
-import java.time.ZoneId.systemDefault
 import java.time.format.DateTimeFormatterBuilder
 
 import scala.util.parsing.combinator.RegexParsers
@@ -16,7 +15,7 @@ object LogParser extends RegexParsers {
     }
   }
   
-  private val formatter = new DateTimeFormatterBuilder().appendPattern("yyyy-MM-dd HH:mm:ss:SSS").toFormatter.withZone(systemDefault())
+  private val utcFormatter = new DateTimeFormatterBuilder().appendPattern("yyyy-MM-dd HH:mm:ss:SSSZ").toFormatter
 
   private def digits2: Parser[Int] ="""\d{2}""".r ^^ {_.toInt}
   private def digits3: Parser[Int] ="""\d{3}""".r ^^ {_.toInt}
@@ -24,13 +23,14 @@ object LogParser extends RegexParsers {
   private def thread: Parser[String] = """\[(.*?)\]""".r
   private def level: Parser[String] = """INFO|WARN|ERROR|TRACE|DEBUG""".r
   private def words: Parser[String] = """(?s:.*)""".r
-    private def instant: Parser[Instant] = {
-    val constituents = digits4 ~ ("-" ~> digits2) ~ ("-" ~> digits2) ~ digits2 ~ (":" ~> digits2) ~ (":" ~> digits2) ~ (":" ~> digits3)
+  
+  def instant: Parser[Instant] = {
+    val constituents = digits4 ~ ("-" ~> digits2) ~ ("-" ~> digits2) ~ digits2 ~ (":" ~> digits2) ~ (":" ~> digits2) ~ (":" ~> digits3) ~ ("+" ~> digits4)
 
     constituents ^^ {
-      case year ~ month ~ day ~ hour ~ mins ~ secs ~ millis => {
-        val string = f"$year-$month%02d-$day%02d $hour%02d:$mins%02d:$secs%02d:$millis%03d"
-        Instant.from(formatter.parse(string))
+      case year ~ month ~ day ~ hour ~ mins ~ secs ~ millis ~ offset => {
+        val string = f"$year-$month%02d-$day%02d $hour%02d:$mins%02d:$secs%02d:$millis%03d+$offset%04d"
+        Instant.from(utcFormatter.parse(string))
       }
     }
   }
