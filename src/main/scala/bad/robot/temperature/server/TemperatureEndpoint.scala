4f06fa9cb9526801b81a71a91cf96ad2aaa74cd2
Toby <toby.weston@gmail.com>
2018-02-27 14:04:30 +0000

    remove unused endpoint (reading temperature directly from sensor)

--- src/main/scala/bad/robot/temperature/server/TemperatureEndpoint.scala
+++ src/main/scala/bad/robot/temperature/server/TemperatureEndpoint.scala
@@ -24,12 +24,6 @@ object TemperatureEndpoint {
   private val lastestTemperatures = CurrentTemperatures(Clock.systemDefaultZone)
 
   def apply(sensors: TemperatureReader, allTemperatures: AllTemperatures, connections: Connections) = HttpService[IO] {
-    // todo delete this one, it shouldn't be used
-    case GET -> Root / "temperature" => {
-      sensors.read.toHttpResponse(measurement => {
-        Ok(s"${measurement.temperatures.average.temperature.asCelsius}")
-      })
-    }
 
     case GET -> Root / "temperatures" / "average" => {
       Ok(encode(lastestTemperatures.average))
