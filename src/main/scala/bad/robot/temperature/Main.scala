5d580132e9b36bbc74e1386e36796099748438ad
Toby <toby.weston@gmail.com>
2018-04-22 19:29:34 +0100

    #57 adding some todos

--- src/main/scala/bad/robot/temperature/Main.scala
+++ src/main/scala/bad/robot/temperature/Main.scala
@@ -3,23 +3,16 @@ package bad.robot.temperature
 import bad.robot.logging.error
 import bad.robot.temperature.client.Client
 import bad.robot.temperature.config.ConfigFile
-import bad.robot.temperature.rrd.Host
 import bad.robot.temperature.server.Server
 import cats.effect.IO
-import fs2.{Stream, StreamApp}
 import fs2.StreamApp.ExitCode
+import fs2.{Stream, StreamApp}
 
 object Main extends StreamApp[IO] {
 
   private val printErrorAndExit = (cause: ConfigurationError) => Stream.eval(error(cause.message)).flatMap(_ => Stream.emit(ExitCode(1)))
 
   override def stream(args: List[String], requestShutdown: IO[Unit]): fs2.Stream[IO, ExitCode] = {
-    
-    // alternative syntax
-    for {
-      c <- Stream.eval(ConfigFile.load()).flatMap(_.fold(printErrorAndExit, start(_)(requestShutdown)))
-    } yield c
-
     Stream
       .eval(ConfigFile.loadOrCreate())
       .flatMap(_.fold(printErrorAndExit, start(_)(requestShutdown)))
@@ -27,9 +20,10 @@ object Main extends StreamApp[IO] {
   
   def start(config: ConfigFile): IO[Unit] => Stream[IO, ExitCode] = {
     config.mode match {
-      case "server" => Server.stream(config.hosts :+ Host.local.name, _)
+      case "server" => Server.stream(config.hosts, _)
       case "client" => Client.stream(Nil, _)
-//      case _ => ??? 
+//      case _ => ???   
+// todo error case?
     }
   }
   
