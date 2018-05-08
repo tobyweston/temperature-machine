4c87ea436b3415a8ae0f94471bab2a9d2aa1987e
Toby <toby.weston@gmail.com>
2018-05-08 19:52:08 +0100

    fix bug with missing $ for finding an ip address

--- src/universal/conf/find_ip.sh
+++ src/universal/conf/find_ip.sh
@@ -1,11 +1,10 @@
 # Pi specific function to try and find the externally facing IP address
 getIpAddress() {
     local eth0=`grep "eth0" /proc/net/dev`
-    if  [ -n "eth0" ] ; then
+    if  [ -n "$eth0" ] ; then
        local lan="eth0"
     else
        local lan="wlan0"
     fi
     echo $( ip -f inet addr show ${lan} | grep -Po 'inet \K[\d.]+' )
 }
-
