bf0f85ba14d6559844df3759967cf857d0a72afd
Toby <toby.weston@gmail.com>
2018-05-15 20:45:40 +0100

    #66 configure Java util logging to output to the log file explicitly

--- src/main/scala/bad/robot/logging/JavaLoggingConfig.scala
+++ src/main/scala/bad/robot/logging/JavaLoggingConfig.scala
@@ -1,8 +1,9 @@
 package bad.robot.logging
 
-import java.util.logging.{Level, Logger}
-import java.util.logging.ConsoleHandler
-import java.util.logging.SimpleFormatter
+import java.util.logging._
+
+import bad.robot.temperature.rrd.RrdFile
+import bad.robot.temperature.FileOps
 
 /**
   * Capture the following properties programmatically
@@ -18,16 +19,24 @@ import java.util.logging.SimpleFormatter
 class JavaLoggingConfig {
 
   System.setProperty("java.util.logging.SimpleFormatter.format", "\u0000%1$tF %1$tH:%1$tM:%1$tS:%1$tL%1$tz [%3$s] %4$s %5$s %n")
-  setHandler(Logger.getLogger("sun.management.jmxremote"), Level.CONFIG, createHandler)
+  setHandler(Logger.getLogger("sun.management.jmxremote"), Level.CONFIG, createConsoleHandler)
+  setHandler(Logger.getLogger("sun.management.jmxremote"), Level.CONFIG, createFileHandler)
 
-  private def createHandler: ConsoleHandler = {
+  private def createFileHandler: Handler = {
+    val file = new FileHandler((RrdFile.path / "temperature-machine.log").getAbsolutePath)
+    file.setLevel(Level.CONFIG)
+    file.setFormatter(new SimpleFormatter)
+    file
+  }
+  
+  private def createConsoleHandler: Handler = {
     val consoleHandler = new ConsoleHandler
     consoleHandler.setLevel(Level.CONFIG)
     consoleHandler.setFormatter(new SimpleFormatter)
     consoleHandler
   }
 
-  private def setHandler(logger: Logger, level: Level, consoleHandler: ConsoleHandler) = {
+  private def setHandler(logger: Logger, level: Level, consoleHandler: Handler) = {
     logger.setLevel(level)
     logger.addHandler(consoleHandler)
   }
