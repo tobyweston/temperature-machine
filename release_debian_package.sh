5bd9bc17c5fd6391c74d54ce3d6fe72cf85ccc78
Toby <toby.weston@gmail.com>
2019-08-31 17:25:37 +0100

    #84 basic script uses a previously created aptly repository (removes dpkg-scanpackages)

--- release_debian_package.sh
+++ release_debian_package.sh
@@ -7,11 +7,17 @@ if ! git diff-index --quiet HEAD --; then
     echo "Outstanding changes, commit or revert with git before deploying the web-app"
     exit -1
 fi
+if [ -z "$1" ] 
+then
+    echo "Usage: ./release_debian_package <pgp passphrase>"
+    exit -1
+fi
 
 RELEASE_FOLDER=target/release-debian-package
 ROOT_FOLDER=$(pwd)
 TARGET_FOLDER=debian
 TARGET_DISTRIBUTION=stable
+APTLY_PUBLISH=debian/aptly/public
 
 # build man pages (prerequisite `gem install ronn` and `gem install rdiscount`)
 echo "Building man pages..."
@@ -19,9 +25,26 @@ cd src/linux/usr/share/man/man1/
 ronn --roff temperature-machine*.md
 cd ${ROOT_FOLDER}
 
-# build package
+# build .deb package
 echo "Building debian package..."
 sbt clean debian:packageBin
+
+# update debian repository (prerequisite required `brew install aptly`)
+# this assumes the repository has previously been created (`aptly create`)
+echo "Adding package to repository..."
+aptly -config=debian/.aptly.conf repo add badrobot-releases ${TARGET_FOLDER}
+
+# update aptly database in git
+git add debian/aptly/db
+git commit -m "updating aptly db after adding new package to the repository" -- debian/aptly/db
+
+echo "Publishing repository..."
+aptly -config=debian/.aptly.conf -distribution=stable -architectures=armhf -gpg-key=00258F48226612AE -passphrase=${1} publish repo badrobot-releases
+# THINK YOU NEED `publish update` here (it doesn't work second time around)
+
+# update aptly database in git
+git add debian/aptly/db
+git commit -m "updating aptly db after (re)publishing" -- debian/aptly/db
 
 # download robotooling
 echo "Downloading robotooling..."
@@ -32,23 +55,13 @@ git clone git@github.com:tobyweston/robotooling.git
 cd robotooling
 git checkout gh-pages
 
-# copy package into robotooling ready to server via HTTP
-echo "Copy package into robotooling..."
-mkdir -p ${TARGET_FOLDER}/${TARGET_DISTRIBUTION}
-cp ${ROOT_FOLDER}/target/temperature-machine_*.deb ${TARGET_FOLDER}/${TARGET_DISTRIBUTION}
-cp ${ROOT_FOLDER}/target/temperature-machine_*.changes ${TARGET_FOLDER}/${TARGET_DISTRIBUTION}
-
-# create debian package file (prerequisite required `brew install dpkg`)
-echo "Create dpkg package..."
-cd ${TARGET_FOLDER}
-dpkg-scanpackages --version
-dpkg-scanpackages -m . > Packages
-gzip -c Packages > Packages.gz
-[ -s Packages ] && echo "Packages file Ok." || { echo "Packages file was empty. Not Ok."; exit 1; }
-
-# git add the new files
-git add .
-git commit -m "update temperature-machine debian package"
+# copy repository into robotooling ready to serve via HTTP
+echo "Copy repository into robotooling gh-pages..."
+git rm -r debian
+mkdir -p debian
+cp -R ../debian/aptly/public/* debian
+git add debian
+git commit -m "updating debian repository (created by aptly)"
 
 # run the robotooling scripts to update html and push the release to gh-pages
 echo "Pushing robotooling to Github gh-pages..."
